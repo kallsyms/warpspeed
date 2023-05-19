7f78dcb0cab90f517503ddeee1f9e6adf527d963
Nick Gregory <computerfreak97@gmail.com>
2023-05-19 03:00:11 -0400

    wip

---
+++ warpspeed/commpage.c
@@ -0,0 +1,75 @@
+// https://github.com/darlinghq/darling/blob/master/src/startup/mldr/commpage.c
+#include "commpage.h"
+#include <sys/mman.h>
+#include <stdio.h>
+#include <errno.h>
+#include <tgmath.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static const char* SIGNATURE32 = "commpage 32-bit";
+static const char* SIGNATURE64 = "commpage 64-bit";
+
+static uint64_t get_cpu_caps(void);
+
+#define CGET(p) (commpage + ((p)-_COMM_PAGE64_BASE_ADDRESS))
+
+void commpage_setup(uint8_t *commpage)
+{
+	uint64_t* cpu_caps64;
+	uint32_t* cpu_caps;
+	uint16_t* version;
+	char* signature;
+	uint64_t my_caps;
+	uint8_t *ncpus, *nactivecpus;
+	uint8_t *physcpus, *logcpus;
+	uint8_t *user_page_shift, *kernel_page_shift;
+
+    fprintf(stderr, "Setting up commpage at %p\n", commpage);
+
+	signature = (char*)CGET(_COMM_PAGE_SIGNATURE);
+	version = (uint16_t*)CGET(_COMM_PAGE_VERSION);
+	cpu_caps64 = (uint64_t*)CGET(_COMM_PAGE_CPU_CAPABILITIES64);
+   	cpu_caps = (uint32_t*)CGET(_COMM_PAGE_CPU_CAPABILITIES);
+
+	strcpy(signature, SIGNATURE64);
+	*version = _COMM_PAGE_THIS_VERSION;
+
+	ncpus = (uint8_t*)CGET(_COMM_PAGE_NCPUS);
+	*ncpus = sysconf(_SC_NPROCESSORS_CONF);
+
+	nactivecpus = (uint8_t*)CGET(_COMM_PAGE_ACTIVE_CPUS);
+	*nactivecpus = sysconf(_SC_NPROCESSORS_ONLN);
+
+	// Better imprecise information than no information
+	physcpus = (uint8_t*)CGET(_COMM_PAGE_PHYSICAL_CPUS);
+	logcpus = (uint8_t*)CGET(_COMM_PAGE_LOGICAL_CPUS);
+	*physcpus = *logcpus = *ncpus;
+
+	// I'm not sure if Linux has seperate page sizes for kernel and user space.
+	// Apple's code uses left shift logical (1 << user_page_shift) to get the page size value.
+	// Since it's very unlikely that the page size won't be a power of 2, we can use __builtin_ctzl()
+	// as a substitute for log2().
+	user_page_shift = (uint8_t*)CGET(_COMM_PAGE_USER_PAGE_SHIFT_64);
+	kernel_page_shift = (uint8_t*)CGET(_COMM_PAGE_KERNEL_PAGE_SHIFT);
+	*kernel_page_shift = *user_page_shift = (uint8_t)__builtin_ctzl(sysconf(_SC_PAGESIZE));
+
+	my_caps = get_cpu_caps();
+	if (*ncpus == 1)
+		my_caps |= kUP;
+
+	*cpu_caps = (uint32_t) my_caps;
+	*cpu_caps64 = my_caps;
+
+    uint64_t* memsize = (uint64_t*)CGET(_COMM_PAGE_MEMORY_SIZE);
+    *memsize = 1 * 1024 * 1024 * 1024;  // ghost: no idea if correct
+}
+
+uint64_t get_cpu_caps(void)
+{
+	uint64_t caps = 0;
+
+    // ghost: TODO
+	return caps;
+}
---
+++ warpspeed/vm.c
@@ -0,0 +1,134 @@
+// simplevm.c: demonstrates Hypervisor.Framework usage in Apple Silicon
+// Based on the work by @zhuowei
+// @imbushuo - Nov 2020
+
+// To build:
+// Prepare the entitlement with BOTH com.apple.security.hypervisor and com.apple.vm.networking WHEN SIP IS OFF
+// Prepare the entitlement com.apple.security.hypervisor and NO com.apple.vm.networking WHEN SIP IS ON
+// ^ Per @never_released, tested on 11.0.1, idk why
+// clang -o simplevm -O2 -framework Hypervisor -mmacosx-version-min=11.0 simplevm.c
+// codesign --entitlements simplevm.entitlements --force -s - simplevm             
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+#include <Hypervisor/Hypervisor.h>
+
+#include "loader.h"
+
+// Diagnostics
+#define HYP_ASSERT_SUCCESS(ret) do { \
+    if ((hv_return_t)(ret) != HV_SUCCESS) { \
+        fprintf(stderr, "%s:%d: %s = %x\n", __FILE__, __LINE__, #ret, (ret)); \
+        abort(); \
+    } \
+} while (0)
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Usage: %s /path/to/bin [args...]", argv[0]);
+        exit(1);
+    }
+
+    struct load_results res = {};
+    load(argv[1], false, &res);
+    setup_stack64(argv[1], &res);
+
+    hv_vcpu_t vcpu;
+    hv_vcpu_exit_t *vcpu_exit;
+
+    HYP_ASSERT_SUCCESS(hv_vm_create(NULL));
+    HYP_ASSERT_SUCCESS(hv_vcpu_create(&vcpu, &vcpu_exit, NULL));
+
+    for (int i = 0; i < res.alloc_n; i++) {
+        int prot = 0;
+        // TODO: necessary?
+        if (res.allocs[i].prot & PROT_READ) {
+            prot |= HV_MEMORY_READ;
+        }
+        if (res.allocs[i].prot & PROT_WRITE) {
+            prot |= HV_MEMORY_WRITE;
+        }
+        if (res.allocs[i].prot & PROT_EXEC) {
+            prot |= HV_MEMORY_EXEC;
+        }
+        fprintf(stderr, "passthru %p %lx %x\n", res.allocs[i].addr, res.allocs[i].len, prot);
+        HYP_ASSERT_SUCCESS(hv_vm_map(res.allocs[i].addr, (hv_ipa_t)res.allocs[i].addr, res.allocs[i].len, prot));
+    }
+
+    // Configure initial VBAR_EL1 to the trampoline
+    //HYP_ASSERT_SUCCESS(hv_vcpu_set_sys_reg(vcpu, HV_SYS_REG_VBAR_EL1, g_kAdrResetTrampoline));
+
+    // Set initial CPSR, PC, and SP
+    // HYP_ASSERT_SUCCESS(hv_vcpu_set_reg(vcpu, HV_REG_CPSR, 0x3c4));
+    fprintf(stderr, "starting at pc=%p, sp=%p\n", res.entry_point, res.stack_top);
+    HYP_ASSERT_SUCCESS(hv_vcpu_set_reg(vcpu, HV_REG_PC, res.entry_point));
+    HYP_ASSERT_SUCCESS(hv_vcpu_set_sys_reg(vcpu, HV_SYS_REG_SP_EL0, res.stack_top));
+
+    HYP_ASSERT_SUCCESS(hv_vcpu_set_trap_debug_exceptions(vcpu, true));
+
+    while (true) {
+        HYP_ASSERT_SUCCESS(hv_vcpu_run(vcpu));
+        if (vcpu_exit->reason == HV_EXIT_REASON_EXCEPTION) {
+            // https://developer.arm.com/documentation/ddi0601/2022-03/AArch64-Registers/ESR-EL2--Exception-Syndrome-Register--EL2-
+            uint64_t syndrome = vcpu_exit->exception.syndrome;
+            uint8_t ec = (syndrome >> 26) & 0x3f;
+            if (ec == 0x16) {
+                // Exception Class 0x16 is
+                // "HVC instruction execution in AArch64 state, when HVC is not disabled."
+                uint64_t x0;
+                HYP_ASSERT_SUCCESS(hv_vcpu_get_reg(vcpu, HV_REG_X0, &x0));
+                printf("VM made an HVC call! x0 register holds 0x%llx\n", x0);
+                break;
+            } else if (ec == 0x17) {
+                // Exception Class 0x17 is
+                // "SMC instruction execution in AArch64 state, when SMC is not disabled."
+
+                uint64_t x0;
+                HYP_ASSERT_SUCCESS(hv_vcpu_get_reg(vcpu, HV_REG_X0, &x0));
+                printf("VM made an SMC call! x0 register holds 0x%llx\n", x0);
+                printf("Return to get on next instruction.\n");
+
+                // ARM spec says trapped SMC have different return path, so it is required
+                // to increment elr_el2 by 4 (one instruction.)
+                uint64_t pc;
+                HYP_ASSERT_SUCCESS(hv_vcpu_get_reg(vcpu, HV_REG_PC, &pc));
+                pc += 4;
+                HYP_ASSERT_SUCCESS(hv_vcpu_set_reg(vcpu, HV_REG_PC, pc));
+            } else if (ec == 0x3C) {
+                // Exception Class 0x3C is BRK in AArch64 state
+                uint64_t x0;
+                HYP_ASSERT_SUCCESS(hv_vcpu_get_reg(vcpu, HV_REG_X0, &x0));
+                printf("VM made an BRK call!\n");
+                printf("Reg dump:\n");
+                for (uint32_t reg = HV_REG_X0; reg < HV_REG_X5; reg++) {
+                    uint64_t s;
+                    HYP_ASSERT_SUCCESS(hv_vcpu_get_reg(vcpu, reg, &s));
+                    printf("X%d: 0x%llx\n", reg, s);
+                }
+                break;
+            } else {
+                fprintf(stderr, "Unexpected VM exception: 0x%llx, EC 0x%x, VirtAddr 0x%llx, IPA 0x%llx\n",
+                    syndrome,
+                    ec,
+                    vcpu_exit->exception.virtual_address,
+                    vcpu_exit->exception.physical_address
+                );
+                break;
+            }
+        } else {
+            fprintf(stderr, "Unexpected VM exit reason: %d\n", vcpu_exit->reason);
+            break;
+        }
+    }
+
+    hv_vcpu_destroy(vcpu);
+    hv_vm_destroy();
+
+    return 0;
+}
